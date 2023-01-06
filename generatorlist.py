e386377b4b02bc9e5283b1e7ec98c9c136447e8c
Denis <den.scherbinin2018@yandex.ru>
2023-01-06 13:47:45 +0500

    add generator list

--- generatorlist.py
+++ generatorlist.py
@@ -1,3 +1,21 @@
 # ГЕНЕРАТОРЫ СПИСКОВ
 
 print('Повторяем генераторы списков.')
+
+
+print([x ** 3 for x in range(10) if x % 2 == 0])
+
+print('-' * 100)
+
+s = []
+
+for i in range(1, 21):
+    if i % 5 == 0:
+        s.append(i)
+
+
+s1 = [i for i  in range(1, 21)
+        if i % 5 == 0]
+
+
+print(f's = {s}; s1 = {s1}')
